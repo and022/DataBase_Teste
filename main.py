7e96e5b69f4d33c2671cf75c2056b018173addd9
Andre Neri Batista <127225532+and022@users.noreply.github.com>
2025-08-04 12:59:07 -0300

    BANCO DE DADOS EM PY
    começando esse simples projeto para a pratica e aprendizado

---
+++ main.py
@@ -0,0 +1,50 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean 
+from sqlalchemy.orm import sessionmaker , declarative_base
+
+
+database = create_engine("sqlite:///mydatabase.db")
+
+Session = sessionmaker(bind=database)
+session = Session()
+
+#criando as tabelas  nesse caso vou criar uma simples cadastro de usuarios e produtos
+#Para isso vou usar Classes init e o Self (padrao do python)
+
+# Criando a classe usuario 
+
+Base_dados = declarative_base()
+
+    # Criando a classe usuario 
+class  Usuario (Base_dados):
+
+    __tablename__ = "usuarios"
+
+    id = Column("id", Integer, primary_key = True)
+    nome_produto = Column("nome", String)
+    email = Column("email", String)
+    senha = Column("senha", String)
+    ativo = Column("status", Boolean)
+
+
+# Criando a classe produtos
+class Produto(Base_dados):
+    __tablename__ = "produto"
+
+    id = Column("id", Integer, primary_key = True)
+    nome_do_produto = Column("nome",String)
+    qtde = Column("qtde", Integer)
+    # IMPORTANDO O foreingKey
+    from sqlalchemy import ForeignKey
+    dono = Column("dono", ForeignKey("usuarios.id"))
+
+Base_dados.metadata.create_all(bind=database)
+
+#criando as tabelas  nesse caso vou criar uma simples cadastro de usuarios e produtos
+#Para isso vou usar Classes init e o Self (padrao do python)
+
+# Criando a classe usuario 
+
+     # 17:48
+
+
+
